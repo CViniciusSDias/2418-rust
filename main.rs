e1f3365f0dc5fc2cefe8f861d67fc15ef7558bfc
Vinicius Dias <carlosv775@gmail.com>
2022-01-15 21:24:23 -0300

    Video 4.3

--- main.rs
+++ main.rs
@@ -54,6 +54,8 @@ fn main() {
     repeticoes();
 
     ownership();
+
+    pattern_matching();
 }
 
 fn condicionais() {
@@ -123,4 +125,16 @@ fn ownership() {
 fn rouba(string: &mut String) {
     string.push_str(" Dias");
     println!("{}", string);
+}
+
+fn pattern_matching() {
+    for x in 1..=20 {
+        println!("{}: {}", x, match x {
+            1 => "Pouco",
+            2 | 3 => "Um pouquinho",
+            4..=10 => "Um bocado",
+            _ if x % 2 == 0 => "Uma boa quantidade",
+            _ => "Muito"
+        });
+    }
 }
